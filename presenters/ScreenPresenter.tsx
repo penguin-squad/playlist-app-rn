911a99318d05698d8389a4c1bd38a435ce4ff8f3
taqui <taqui@pop-os.localdomain>
2021-05-01 11:58:44 +0200

    Fixed Firebase, Added StartPage, Fixed Store, Added Thunk

---
+++ presenters/ScreenPresenter.tsx
@@ -0,0 +1,20 @@
+import React, { FC , useState } from "react";
+import { Pressable } from "react-native";
+import { StyleSheet } from "react-native";
+import {View, Button, Modal } from "react-native";
+import { Text, TextInput } from "../components/Themed";
+
+
+
+
+const startPage = (props) => {
+ return (
+  <View>
+
+  </View>
+    );
+};
+
+export default startPage;
+
+
