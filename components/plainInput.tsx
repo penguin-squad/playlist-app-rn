c2e30ad986536550cc148aeab602eac71ea647cb
taqui <taqui@pop-os.localdomain>
2021-05-11 19:24:24 +0200

    Fixed Input Reset after add, Fixed Search

--- components/plainInput.tsx
+++ components/plainInput.tsx
@@ -7,6 +7,7 @@ interface Props {
     placeholder: string;
     onChangeText: (text: string) => void;
     blurOnSubmit: boolean;
+    value: string;
 }
 
 
@@ -18,6 +19,7 @@ const PlainInput = (props: Props) => (
           placeholder={props.placeholder} 
           onChangeText={props.onChangeText}
           blurOnSubmit ={props.blurOnSubmit}
+          value={props.value}
           />
        </View>            
     );
